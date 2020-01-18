39e68a77656651ae2c2b8ec839f1eb5244ed6fa1
Ivan Sohrab Gill <ivan.gill@bccdc.ca>
2020-01-17 17:10:35 -0800

    Exclude tests from package installation

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/ivansg44/quicktree",
-    packages=setuptools.find_packages(),
+    packages=setuptools.find_packages(exclude=["tests"]),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
