ae2a5c5b9154b04bbeeb9a0e46f9806b369b333c
Ivan Sohrab Gill <ivan.gill@bccdc.ca>
2020-09-05 11:35:29 -0700

    Implement test suite

--- tests/test.py
+++ tests/test.py
@@ -277,8 +277,168 @@ class TestMapQuickTreeDirs(unittest.TestCase):
 
 
 class TestGetOutputStr(unittest.TestCase):
-    def test_stub(self):
-        self.assertTrue(False)
+    @staticmethod
+    def _get_test_dir_path(dir_name):
+        return TestGetQuickTreeStruct._get_test_dir_path(dir_name)
+
+    @staticmethod
+    def _get_alpha_num_order():
+        return ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b",
+                "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n",
+                "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
+    def test_empty(self):
+        test_dir_path = self._get_test_dir_path("test_empty")
+        struct = helpers.get_quicktree_struct(test_dir_path)
+        map = helpers.map_quicktree_dirs(struct)
+        actual_output_str = helpers.get_output_str(struct, map)
+        expected_output_str = "Current Directory\n" \
+                              "\n" \
+                              "Folders\n" \
+                              "\n"
+        self.assertEqual(expected_output_str, actual_output_str)
+
+    def test_one_file(self):
+        test_dir_path = self._get_test_dir_path("test_one_file")
+        struct = helpers.get_quicktree_struct(test_dir_path)
+        map = helpers.map_quicktree_dirs(struct)
+        actual_output_str = helpers.get_output_str(struct, map)
+        expected_output_str = "Current Directory\n" \
+                              "spam\n" \
+                              "Folders\n" \
+                              "\n"
+        self.assertEqual(expected_output_str, actual_output_str)
+
+    def test_multi_files(self):
+        test_dir_path = self._get_test_dir_path("test_multi_files")
+        struct = helpers.get_quicktree_struct(test_dir_path)
+        map = helpers.map_quicktree_dirs(struct)
+        actual_output_str = helpers.get_output_str(struct, map)
+        expected_output_str = "Current Directory\n" \
+                              "eggs ham spam\n" \
+                              "Folders\n" \
+                              "\n"
+        self.assertEqual(expected_output_str, actual_output_str)
+
+    def test_one_dir(self):
+        test_dir_path = self._get_test_dir_path("test_one_dir")
+        struct = helpers.get_quicktree_struct(test_dir_path)
+        map = helpers.map_quicktree_dirs(struct)
+        actual_output_str = helpers.get_output_str(struct, map)
+        expected_output_str = "Current Directory\n" \
+                              "\n" \
+                              "Folders\n" \
+                              "0: foo\n"
+        self.assertEqual(expected_output_str, actual_output_str)
+
+    def test_multi_dirs(self):
+        test_dir_path = self._get_test_dir_path("test_multi_dirs")
+        struct = helpers.get_quicktree_struct(test_dir_path)
+        map = helpers.map_quicktree_dirs(struct)
+        actual_output_str = helpers.get_output_str(struct, map)
+        expected_output_str = "Current Directory\n" \
+                              "\n" \
+                              "Folders\n" \
+                              "0: bar 1: foo\n"
+        self.assertEqual(expected_output_str, actual_output_str)
+
+    def test_one_file_one_dir(self):
+        test_dir_path = self._get_test_dir_path("test_one_file_one_dir")
+        struct = helpers.get_quicktree_struct(test_dir_path)
+        map = helpers.map_quicktree_dirs(struct)
+        actual_output_str = helpers.get_output_str(struct, map)
+        expected_output_str = "Current Directory\n" \
+                              "spam\n" \
+                              "Folders\n" \
+                              "0: foo\n"
+        self.assertEqual(expected_output_str, actual_output_str)
+
+    def test_multi_files_one_dir(self):
+        test_dir_path = self._get_test_dir_path("test_multi_files_one_dir")
+        struct = helpers.get_quicktree_struct(test_dir_path)
+        map = helpers.map_quicktree_dirs(struct)
+        actual_output_str = helpers.get_output_str(struct, map)
+        expected_output_str = "Current Directory\n" \
+                              "eggs ham spam\n" \
+                              "Folders\n" \
+                              "0: foo\n"
+        self.assertEqual(expected_output_str, actual_output_str)
+
+    def test_one_file_multi_dirs(self):
+        test_dir_path = self._get_test_dir_path("test_one_file_multi_dirs")
+        struct = helpers.get_quicktree_struct(test_dir_path)
+        map = helpers.map_quicktree_dirs(struct)
+        actual_output_str = helpers.get_output_str(struct, map)
+        expected_output_str = "Current Directory\n" \
+                              "spam\n" \
+                              "Folders\n" \
+                              "0: foo 1: bar\n"
+        self.assertEqual(expected_output_str, actual_output_str)
+
+    def test_multi_files_multi_dirs(self):
+        test_dir_path = self._get_test_dir_path("test_multi_files_multi_dirs")
+        struct = helpers.get_quicktree_struct(test_dir_path)
+        map = helpers.map_quicktree_dirs(struct)
+        actual_output_str = helpers.get_output_str(struct, map)
+        expected_output_str = "Current Directory\n" \
+                              "eggs ham spam\n" \
+                              "Folders\n" \
+                              "0: foo 1: bar\n"
+        self.assertEqual(expected_output_str, actual_output_str)
+
+    def test_36_dirs(self):
+        test_dir_path = self._get_test_dir_path("test_36_dirs")
+        struct = helpers.get_quicktree_struct(test_dir_path)
+        map = helpers.map_quicktree_dirs(struct)
+        actual_output_str = helpers.get_output_str(struct, map)
+        expected_output_str = "Current Directory\n" \
+                              "eggs ham spam\n" \
+                              "Folders\n"
+
+        alpha_num_order = self._get_alpha_num_order()
+        sorted_dirs = sorted(range(36), key=str)
+        for i in range(len(alpha_num_order)):
+            expected_output_str += alpha_num_order[i] + ": "
+            expected_output_str += str(sorted_dirs[i]) + " "
+        expected_output_str = expected_output_str[:-1]
+
+        self.assertEqual(expected_output_str, actual_output_str)
+
+    def test_37_dirs(self):
+        test_dir_path = self._get_test_dir_path("test_37_dirs")
+        struct = helpers.get_quicktree_struct(test_dir_path)
+        map = helpers.map_quicktree_dirs(struct)
+        actual_output_str = helpers.get_output_str(struct, map)
+        expected_output_str = "Current Directory\n" \
+                              "eggs ham spam\n" \
+                              "Folders\n"
+
+        alpha_num_order = self._get_alpha_num_order()
+        sorted_dirs = sorted(range(37), key=str)
+        for i in range(len(alpha_num_order)):
+            expected_output_str += alpha_num_order[i] + ": "
+            expected_output_str += str(sorted_dirs[i]) + " "
+        expected_output_str += "...\n"
+
+        self.assertEqual(expected_output_str, actual_output_str)
+
+    def test_73_dirs(self):
+        test_dir_path = self._get_test_dir_path("test_73_dirs")
+        struct = helpers.get_quicktree_struct(test_dir_path)
+        map = helpers.map_quicktree_dirs(struct)
+        actual_output_str = helpers.get_output_str(struct, map)
+        expected_output_str = "Current Directory\n" \
+                              "eggs ham spam\n" \
+                              "Folders\n"
+
+        alpha_num_order = self._get_alpha_num_order()
+        sorted_dirs = sorted(range(73), key=str)
+        for i in range(len(alpha_num_order)):
+            expected_output_str += alpha_num_order[i] + ": "
+            expected_output_str += str(sorted_dirs[i]) + " "
+        expected_output_str += "...\n"
+
+        self.assertEqual(expected_output_str, actual_output_str)
 
 
 if __name__ == "__main__":
